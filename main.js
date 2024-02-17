b973fc039f52eda1c910d3c79289b514267a53c0
sebaJordanYT <sebastiajordanyt@gmail.com>
2024-02-17 19:28:32 -0300

    efectos scroll

---
+++ main.js
@@ -0,0 +1,9 @@
+/*
+Para la frase con autoscroll en vez de repetir el div de html para que parezca infinito puedo hacer esta función que clona
+(es decir solo dejaría un div)
+
+var copy = document.querySelector(".logos-slide").cloneNode(true);
+document.querySelector('.logos').appendChild(copy);
+*/
+
+
